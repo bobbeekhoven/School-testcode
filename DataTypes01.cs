4fd57e7a68c16b18843b9565b794e9d15f244355
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-03-06 15:02:06 +0100

    Add files via upload

---
+++ DataTypes01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes01
+{
+    class DataTypes01
+    {
+        static void Main(string[] args)
+        {
+            int I;
+            I = 5;
+            DateTime dt = DateTime.Now;
+            DateTime dFuture = dt.AddDays(I);
+            int J = I * 5;
+            string S = "This is a string";
+            // Next 2 lines are the same but with a different method of notation
+            J = J + 6;
+            J += 6;
+            J++;      // ++ means +1
+            Console.WriteLine(I);
+            Console.WriteLine(J);
+            Console.WriteLine(S);
+            Console.WriteLine(dFuture);
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
